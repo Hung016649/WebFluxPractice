24a8f04ea18bef8c783c864fb4f7c66e459e6c21
HungLT Local <hung01664948012@gmail.com>
2024-09-10 21:56:42 +0700

    First Commit

---
+++ WebFluxPractice/src/main/java/com/thuyhoang/WebFluxPractice/WebFluxPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.thuyhoang.WebFluxPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebFluxPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebFluxPracticeApplication.class, args);
+	}
+
+}
